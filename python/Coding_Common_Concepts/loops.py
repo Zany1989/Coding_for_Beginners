35d26a910f4beeac103e6c50c42461435dada7ed
Zane Zeltina <zanezeltina32@inbox.lv>
2026-04-06 15:58:18 +0300

    Add Python file: loops
    Add Python file what containing basic loop demonstrations. Includes a while loop that counts from 0 to 4 and prints "Finished!", and a for loop that iterates over a sample shopping list (myShoppingList), printing each item. Marked as V1.0 for simple instructional use.

---
+++ python/Coding_Common_Concepts/loops.py
@@ -0,0 +1,20 @@
+'''Python counting script
+V1.0'''
+
+# Here we use while loop
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1 # This is the same as count plus one.
+print("Finished!")
+
+'''Python items list
+V1.0'''
+
+# Here we use for loop
+
+myShoppingList = ["Apples", "Oranges", "Bread", "Milk"]
+for item in myShoppingList:
+    print(item) # Here we see a list of items.
+
